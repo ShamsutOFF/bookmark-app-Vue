f04fc710567fb2759acd0efd64297aada9c479d7
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-09 15:03:32 +0300

    11.9. Создание категории

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useCategoryStore } from '@/stores/categories.store.ts';
 import { onMounted } from 'vue';
+import ButtonIcon from '@/components/ButtonIcon.vue';
+import IconPlus from '@/icons/IconPlus.vue';
 
 const store = useCategoryStore();
 onMounted(() => {
@@ -12,6 +14,11 @@ onMounted(() => {
   <ul class="category-list">
     <li class="category-list__item" v-for="category in store.categories" :key="category.id">
       <a :href="`/${category.alias}`">{{ category.name }} </a>
+    </li>
+    <li>
+      <ButtonIcon @click="store.createCategory()">
+        <IconPlus />
+      </ButtonIcon>
     </li>
   </ul>
 </template>
@@ -26,6 +33,10 @@ onMounted(() => {
 }
 
 .category-list__item {
+  list-style: none;
+}
+
+.category-list li {
   list-style: none;
 }
 
--- src/stores/categories.store.ts
+++ src/stores/categories.store.ts
@@ -5,9 +5,19 @@ import { API_ROUTES, http } from '@/api.ts';
 
 export const useCategoryStore = defineStore('categories', () => {
   const categories = ref<ICategory[]>([]);
+
   async function fetchCategories() {
     const { data } = await http.get<ICategory[]>(API_ROUTES.categories);
     categories.value = data;
   }
-  return { categories, fetchCategories };
+
+  async function createCategory() {
+    const { data } = await http.post<ICategory>(API_ROUTES.categories,{
+      name: 'Новая категория',
+      alias: 'new-category',
+    });
+    categories.value.push(data);
+  }
+
+  return { categories, fetchCategories, createCategory };
 });
