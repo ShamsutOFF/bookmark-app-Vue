ce26eb64106805923f6d283d60b54a8b308dcb15
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-11 14:05:25 +0300

    13.2. Работа с формой

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import ButtonText from '@/components/ButtonText.vue';
-import { useRouter } from 'vue-router';
 import InputString from '@/components/InputString.vue';
 import { ref } from 'vue';
 
-const router = useRouter();
-const email = ref<string>();
-function redirectToMain() {
-  router.push({ name: 'main' });
+const form = ref<{ email?: string; password?: string }>({});
+function onSubmit(event: Event) {
+  event.preventDefault();
 }
 </script>
 
@@ -15,8 +13,11 @@ function redirectToMain() {
   <div class="auth">
     <div class="auth__form">
       <h1 class="auth__header">Bookmarkly</h1>
-      <InputString v-model="email" placeholder="Email" type="email" />
-      <ButtonText @click="redirectToMain">Вход</ButtonText>
+      <form class="auth__form" @submit="onSubmit">
+        <InputString v-model="form.email" placeholder="Email" type="email" />
+        <InputString v-model="form.password" placeholder="Password" type="password" />
+        <ButtonText type="submit">Вход</ButtonText>
+      </form>
     </div>
   </div>
 </template>
